819f8f031ae0425fef937c9c769253fd85eb90f5
Nataliia <natalabojko@gmail.com>
2024-10-08 17:09:19 +0300

    start header

--- src/components/Buttons/ButtonLink/ButtonLink.jsx
+++ src/components/Buttons/ButtonLink/ButtonLink.jsx
@@ -1,14 +1,12 @@
 import Link from "next/link";
 import styles from "./ButtonLink.module.scss";
 
-
 const ButtonLink = ({ customBtn, href, title }) => {
-    return (
-        <Link href={href} className={styles.btn + ' ' + customBtn} >
-            {title}
-        </Link>
-    );
+  return (
+    <Link href={href} className={styles.btn + " " + customBtn}>
+      {title}
+    </Link>
+  );
 };
-
 
 export default ButtonLink;
