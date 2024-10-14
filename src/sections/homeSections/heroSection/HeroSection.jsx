b31c61e7d65f4b9fe2e7c78af0a1ea4455b46a17
Nataliia <natalabojko@gmail.com>
2024-10-14 18:54:50 +0300

    Hero and About titles optimisation

--- src/sections/homeSections/heroSection/HeroSection.jsx
+++ src/sections/homeSections/heroSection/HeroSection.jsx
@@ -5,18 +5,36 @@ const HeroSection = () => {
   return (
     <section id="hero" className={styles.hero}>
       <div className={`container ${styles.container}`}>
-        <h1 className={styles.title}>
-          Сучасний поліграф (детектор брехні) у Львові: точність та новітні
-          технології EyeDetect
-        </h1>
-        <h2 className={styles.text}>
-          Поліграф-тестування у Львові: швидка та достовірна перевірка за
-          допомогою технології EyeDetect
-        </h2>
+        <div className={styles.titleWrapp}>
+          <h2 className={styles.title}>
+            Передові рішення для виявлення обману швидко і точно
+          </h2>
+          <h3 className={styles.text}>
+            швидка та достовірна перевірка за допомогою технології eyedetect
+          </h3>
+        </div>
         <OpenModalBtn customBtn={styles.btn} />
       </div>
     </section>
   );
 };
+
+// const HeroSection = () => {
+//   return (
+//     <section id="hero" className={styles.hero}>
+//       <div className={`container ${styles.container}`}>
+//         <h1 className={styles.title}>
+//           Сучасний поліграф (детектор брехні) у Львові: точність та новітні
+//           технології EyeDetect
+//         </h1>
+//         <h2 className={styles.text}>
+//           Поліграф-тестування у Львові: швидка та достовірна перевірка за
+//           допомогою технології EyeDetect
+//         </h2>
+//         <OpenModalBtn customBtn={styles.btn} />
+//       </div>
+//     </section>
+//   );
+// };
 
 export default HeroSection;
