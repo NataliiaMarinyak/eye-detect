5487e2467fbf1c80033b58c874745af4f44d1d4d
Nataliia <natalabojko@gmail.com>
2024-10-16 22:33:55 +0300

    SpecialistSection

--- src/sections/specialistSection/SpecialistSection.jsx
+++ src/sections/specialistSection/SpecialistSection.jsx
@@ -1,8 +1,55 @@
+import { spesialistData } from "@/data/spesialistData";
+import styles from "./SpecialistSection.module.scss";
+import Image from "next/image";
+
 const SpecialistSection = () => {
-    return (
-        <section>Specialist Section</section>
-    )
-}
+  return (
+    <section>
+      <div className={`container ${styles.container}`}>
+        <h2 className={styles.title}>Тестувальник EyeDetect Наталія Мариняк</h2>
 
+        <ul>
+          {spesialistData.map((el, i) => {
+            return (
+              <li key={i} className={styles.contentWrapp}>
+                <div className={styles.textWrapp}>
+                  <h3 className={styles.subtitle}>{el.title}</h3>
+                  <ul>
+                    {el.details.map((det, i) => {
+                      return (
+                        <li key={i} className={styles.details}>
+                          <h4 className={styles.detailsTitle}>
+                            <svg>
+                              <use href="./sprite.svg#icon-check-mark-in-circle"></use>
+                            </svg>
 
-export default SpecialistSection
+                            {det.subtitle}
+                          </h4>
+                          <ul className={styles.detailsList}>
+                            {det.list.map((item, i) => {
+                              return <li key={i}>{item}</li>;
+                            })}
+                          </ul>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+                <div className={styles.imgWrapp}>
+                  <Image
+                    src={el.img}
+                    alt={el.title}
+                    fill
+                    sizes="(max-width: 1023px) 100vw, (max-width: 1440px) 50vw, 33vw"
+                  />
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default SpecialistSection;
