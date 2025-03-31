4b094a13ad4077d0016e33f4215f0d4e8e051bff
Nataliia <natalabojko@gmail.com>
2025-03-31 15:41:17 +0300

    abourSectiomFixing

--- src/sections/specialistSection/SpecialistSection.jsx
+++ src/sections/specialistSection/SpecialistSection.jsx
@@ -6,26 +6,27 @@ import { specialistData } from "@/data/specialistData";
 import { languagesData } from "@/data/languagesData";
 import styles from "./SpecialistSection.module.scss";
 
-
 const SpecialistSection = () => {
   const { t, i18n } = useTranslation();
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => setIsLoading(false), []);
 
-
   return (
     <section>
       <div className="container">
-        <h2 className={styles.title}>{!isLoading && t("SpecialistSection.Title")}</h2>
+        <h2 className={styles.title}>
+          {!isLoading && t("SpecialistSection.Title")}
+        </h2>
         <ul>
           {specialistData.map((el, i) => {
             return (
               <li key={i} className={styles.contentWrapp}>
                 <div className={styles.textWrapp}>
                   <h3 className={styles.subtitle}>
-                    {!isLoading && (i18n.language === languagesData.UA
-                      ? el.title
-                      : el.titleRus)}
+                    {!isLoading &&
+                      (i18n.language === languagesData.UA
+                        ? el.title
+                        : el.titleRus)}
                   </h3>
                   <ul>
                     {el.details.map((det, i) => {
@@ -35,37 +36,71 @@ const SpecialistSection = () => {
                             <svg>
                               <use href="/sprite.svg#icon-check-mark-in-circle"></use>
                             </svg>
-                            {!isLoading && (i18n.language === languagesData.UA
-                              ? det.subTitle
-                              : det.subTitleRus)}
+                            {!isLoading &&
+                              (i18n.language === languagesData.UA
+                                ? det.subTitle
+                                : det.subTitleRus)}
                           </h4>
                           <ul className={styles.detailsList}>
-                            {!isLoading && (i18n.language === languagesData.UA
-                              ? det.list.map((item, i) => {
-                                return <li key={i}>{item}</li>;
-                              })
-                              : det.listRus.map((item, i) => {
-                                return <li key={i}>{item}</li>;
-                              }))
-                            }
+                            {!isLoading &&
+                              (i18n.language === languagesData.UA
+                                ? det.list.map((item, i) => {
+                                    return <li key={i}>{item}</li>;
+                                  })
+                                : det.listRus.map((item, i) => {
+                                    return <li key={i}>{item}</li>;
+                                  }))}
                           </ul>
                         </li>
                       );
                     })}
                   </ul>
                 </div>
-                <div className={styles.imgWrapp}>
-                  {!isLoading && <Image
-                    src={el.img}
-                    alt={i18n.language === languagesData.UA
-                      ? el.title
-                      : el.titleRus}
-                    width={943}
-                    height={638}
-                    sizes="(max-width: 1023px) 100vw, (max-width: 1440px) 50vw, 375px"
-                    loading="eager"
-                  />}
-                </div>
+
+                {el.imgCaption ? (
+                  <figure
+                    className={`${styles.imgWrapp} ${styles.imgWrappFigure}`}
+                  >
+                    {!isLoading && (
+                      <Image
+                        src={el.img}
+                        alt={
+                          i18n.language === languagesData.UA
+                            ? el.title
+                            : el.titleRus
+                        }
+                        width={943}
+                        height={638}
+                        sizes="(max-width: 1023px) 100vw, (max-width: 1440px) 50vw, 375px"
+                        loading="eager"
+                      />
+                    )}
+
+                    <figcaption className={styles.imgCaption}>
+                      {!isLoading &&
+                        (i18n.language === languagesData.UA
+                          ? el.imgCaption.Ua
+                          : el.imgCaption.Ru)}
+                    </figcaption>
+                  </figure>
+                ) : (
+                  <div className={styles.imgWrapp}>
+                    {!isLoading && (
+                      <Image
+                        src={el.img}
+                        alt={
+                          i18n.language === languagesData.UA
+                            ? el.title
+                            : el.titleRus
+                        }
+                        width={943}
+                        height={638}
+                        sizes="(max-width: 1023px) 100vw, (max-width: 1440px) 50vw, 375px"
+                        loading="eager"
+                      />
+                    )}
+                  </div>
+                )}
               </li>
             );
           })}
