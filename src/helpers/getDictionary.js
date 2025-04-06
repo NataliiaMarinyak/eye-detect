62d86ecdefe22c0f535af0425d5de0571d794b26
volodymirkavun1 <volodymirkavun1@gmail.com>
2025-04-06 22:57:44 +0300

    modified: file_getDictionary

--- src/helpers/getDictionary.js
+++ src/helpers/getDictionary.js
@@ -1,8 +1,24 @@
+// import "server-only";
+
+// const dictionaries = {
+//   uk: () => import("@/dictionaries/uk.json").then((module) => module.default),
+//   ru: () => import("@/dictionaries/ru.json").then((module) => module.default),
+// };
+
+// export const getDictionary = async (locale) => dictionaries[locale]();
+
+
 import "server-only";
 
 const dictionaries = {
-  uk: () => import("@/dictionaries/uk.json").then((module) => module.default),
-  ru: () => import("@/dictionaries/ru.json").then((module) => module.default),
+  uk: () => import("@/dictionaries/uk.json").then((m) => m.default),
+  ru: () => import("@/dictionaries/ru.json").then((m) => m.default),
 };
 
-export const getDictionary = async (locale) => dictionaries[locale]();
+export const getDictionary = async (locale) => {
+  const loadDictionary = dictionaries[locale];
+  if (!loadDictionary) {
+    throw new Error(`Unsupported locale: ${locale}`);
+  }
+  return loadDictionary();
+};
