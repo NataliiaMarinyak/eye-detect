7de5d1a34cd4f7d3e562ee43c55fb57d7467084a
NataliiaMarinyak <nataliiamarinyak@gmail.com>
2026-05-24 00:30:05 +0300

    i18n: add 'en' locale to supported languages

--- src/dictionaries/i18n.config.js
+++ src/dictionaries/i18n.config.js
@@ -1,10 +1,9 @@
 // export const i18n = {
-//   defaultLocale: "uk",
-//   locales: ["uk", "ru"],
+// defaultLocale: "uk",
+// locales: ["uk", "ru"],
 // };
 
-
 export const i18n = {
-  locales: ['uk', 'ru'],
+  locales: ['uk', 'ru', 'en'],
   defaultLocale: 'uk',
 };
