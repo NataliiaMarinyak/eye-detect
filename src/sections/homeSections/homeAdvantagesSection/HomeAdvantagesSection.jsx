7fafedf9b05640e8aaf51d34bb3bc9f67dad0516
NataliiaMarinyak <nataliiamarinyak@gmail.com>
2026-05-23 19:45:34 +0300

    Render advantages with title and subTitle

--- src/sections/homeSections/homeAdvantagesSection/HomeAdvantagesSection.jsx
+++ src/sections/homeSections/homeAdvantagesSection/HomeAdvantagesSection.jsx
@@ -7,25 +7,28 @@ import { i18n } from "@/dictionaries/i18n.config";
 import styles from "./HomeAdvantagesSection.module.scss";
 
 const HomeAdvantagesSection = ({ lang, dictionary }) => {
-  // const { t, i18n } = useTranslation();
-  // const [isLoading, setIsLoading] = useState(true);
-  // useEffect(() => setIsLoading(false), []);
-  // console.log("lang in advantage", lang);
-
   return (
     <section>
       <div className={`container ${styles.container}`}>
         <h2 className={styles.title}>
           {dictionary.homeAdvantagesSection.title}
         </h2>
+        {dictionary.homeAdvantagesSection.subTitle && (
+          <p className={styles.subTitle}>
+            {dictionary.homeAdvantagesSection.subTitle}
+          </p>
+        )}
         <ul className={styles.advantagesWrapper}>
           {advantagesData.map((advantage) => (
-            <li key={advantage.text} className={styles.advantageItem}>
+            <li key={advantage.title} className={styles.advantageItem}>
               <div className={styles.iconWrapper}>
                 <svg className={styles.icon}>
                   <use href={`/sprite.svg#icon-check-mark-in-circle`} />
                 </svg>
               </div>
+              <h3 className={styles.advantageTitle}>
+                {lang === i18n.locales[0] ? advantage.title : advantage.titleRus}
+              </h3>
               <p className={styles.description}>
                 {lang === i18n.locales[0] ? advantage.text : advantage.textRus}
               </p>
