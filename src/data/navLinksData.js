382f4eb71950bd32a085f3bcc6c65747854673e1
NataliiaMarinyak <nataliiamarinyak@gmail.com>
2026-05-24 00:56:07 +0300

    navLinksData: add titleEng for nav items (Home/About/Locations/Contact)

--- src/data/navLinksData.js
+++ src/data/navLinksData.js
@@ -2,21 +2,25 @@ export const navLinksData = [
   {
     title: "Головна",
     titleRus: "Главная",
+    titleEng: "Home",
     href: "/",
   },
   {
     title: "Про нас",
     titleRus: "О нас",
+    titleEng: "About",
     href: "/about-us",
   },
   {
     title: "Локації",
     titleRus: "Локации",
+    titleEng: "Locations",
     href: "/locations",
   },
   {
     title: "Контакти",
     titleRus: "Контакты",
+    titleEng: "Contact",
     href: "/contacts",
   },
 ];
