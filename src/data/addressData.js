b2e76adc992a766b6cffc9f5864a347257246f17
NataliiaMarinyak <nataliiamarinyak@gmail.com>
2026-08-27 10:01:38 +0300

    content: адреса Городоцька 45 + карта на Google-картку сайту

--- src/data/addressData.js
+++ src/data/addressData.js
@@ -1,10 +1,12 @@
 export const addressData = {
-  hrefLink: "https://maps.app.goo.gl/dP8aPY264zdKpEoT6",
-  textAddress: "Львів, вул. Данилишина 6, оф. 217",
-  textAddressRus: "Львов, ул. Данилишина 6, оф. 217",
-  textAddressEng: "Lviv, 6 Danylyshyna St., office 217",
+  // Посилання веде на картку Google Business "Детектор брехні Львів. Поліграф EyeDetect"
+  // (cid=14456841015853740907), яка прив'язана саме до цього сайту.
+  hrefLink: "https://maps.google.com/?cid=14456841015853740907",
+  textAddress: "Львів, вул. Городоцька, 45",
+  textAddressRus: "Львов, ул. Городоцкая, 45",
+  textAddressEng: "Lviv, 45 Horodotska St.",
   iframeSrcLink:
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2572.9463559329574!2d24.02055367655992!3d49.843465471482666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473add739d964237%3A0x45b9239b44ba1e3b!2z0LLRg9C70LjRhtGPINCU0LzQuNGC0YDQsCDQlNCw0L3QuNC70LjRiNC40L3QsCwgNiwg0JvRjNCy0ZbQsiwg0JvRjNCy0ZbQstGB0YzQutCwINC-0LHQu9Cw0YHRgtGMLCA3OTAwMA!5e0!3m2!1suk!2sua!4v1728562646496!5m2!1suk!2sua",
+    "https://www.google.com/maps/embed?origin=mfe&pb=!1m3!2m1!1s49.8426459,24.0194475!6i17!3m1!1suk!5m1!1suk",
   siteLink: "https://www.eye-polygraph.com/",
   siteDomain: "eye-polygraph.com/",
 };
